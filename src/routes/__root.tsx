e845cb802e5dccd3a69c136c45e9d7182904a67f
itswil <ww8668@gmail.com>
2024-11-21 17:24:46 +0800

    Add queryClient to context types

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,23 @@
-import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import type { QueryClient } from "@tanstack/react-query";
+import {
+	Link,
+	Outlet,
+	createRootRouteWithContext,
+} from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import * as React from "react";
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient;
+}>()({
 	component: RootComponent,
+	notFoundComponent: () => {
+		return (
+			<div>
+				<p>This is the notFoundComponent configured on root route</p>
+				<Link to="/">Start Over</Link>
+			</div>
+		);
+	},
 });
 
 function RootComponent() {
